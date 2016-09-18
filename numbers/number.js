04498d1d1daf9c93946e62998aa59612b6c636d1
flipps <fi.morais.mail@gmail.com>
2016-09-18 11:11:06 -0300

    Numbers commit.

---
+++ numbers/number.js
@@ -0,0 +1,42 @@
+// var age = 29;
+// var results;
+//
+// results = age + 6;
+// console.log('age + 6 = ' + results);
+//
+// results = age - 1;
+// console.log('age - 1 = ' + results );
+//
+// results = age * 2;
+// console.log('age * 2 = ' + results);
+//
+// results = age / 2;
+// console.log('age / 2 = ' + results);
+//
+// results = age % 5;
+// console.log('age % 5 = ' + results);
+//
+// results = results + 1;
+// console.log('results = results + 1 = ' + results);
+//
+// results = results - 1;
+// console.log('results = results - 1 = ' + results);
+//
+// results ++;
+// console.log('results ++ = ' + results);
+//
+// results --;
+// console.log('results -- = ' + results);
+//
+// var num = 19;
+// console.log(++num); //increments num before console.
+// console.log(num++); //increments num after console.
+// console.log(num);
+
+//Exercise:
+var gradeOne = 70;
+var gradeTwo = 50;
+var gradeThree = 90;
+var averageGrade = (gradeOne + gradeTwo + gradeThree) / 3;
+
+console.log('Your average grade is: ' + averageGrade);
