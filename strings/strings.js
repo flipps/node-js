5b0f6de183ff4f41e1a99ccdcf1b92cbd94c6496
flipps <fi.morais.mail@gmail.com>
2016-09-18 10:40:47 -0300

    Strings commit.

---
+++ strings/strings.js
@@ -0,0 +1,13 @@
+var name = 'Filipe';
+var lastName = 'Morais';
+console.log(name + ' ' + lastName);
+console.log(lastName.length);
+
+//
+
+var firstName = 'Filipe';
+var lastName = 'Morais';
+var fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+console.log(fullName.length);
