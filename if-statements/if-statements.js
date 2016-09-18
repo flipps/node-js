f471c3e78eac3bca21dacb0f14974664e487615c
flipps <fi.morais.mail@gmail.com>
2016-09-18 11:36:05 -0300

    If statements commit.

---
+++ if-statements/if-statements.js
@@ -0,0 +1,19 @@
+// var averageGrade = 100;
+//
+// if (averageGrade === 100) {
+//   console.log('You were awesome! Congratulations!');
+// } else if(averageGrade >= 70) {
+//     console.log('Congratulations! You passed the class.');
+// }  else {
+//     console.log('Im sorry you failed the class.');
+// }
+
+var friend = 'Vibs';
+
+if (friend === 'Joe' || friend === 'Gee' || friend === 'Vibs') {
+  console.log('hello ' + friend + '!');
+} else if (friend === 'Temer' || friend === 'Inacio' ) {
+  console.log('what do you want ' + friend + '?');
+} else {
+  console.log('Hey stranger.');
+}
