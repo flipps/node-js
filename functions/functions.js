540994853bf6722af867e95d1d01f8d4ceb4a85a
flipps <fi.morais.mail@gmail.com>
2016-09-22 00:31:02 -0300

    Functions commit.

---
+++ functions/functions.js
@@ -0,0 +1,15 @@
+// function greetUser (name, lastName) {
+//   return 'Hello ' + name + ' ' + lastName + '!';
+// }
+//
+// var greeting = greetUser('Filipe', 'Morais');
+// console.log(greeting);
+
+function add (a, b) {
+  return a + b;
+}
+
+var result = add (5, 5);
+result = add (10, 3);
+console.log(result);
+console.log(add (5, 17));
