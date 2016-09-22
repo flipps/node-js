3a0a83d8a6384073204d6646cc8ae3b94bc9b8ef
flipps <fi.morais.mail@gmail.com>
2016-09-22 00:53:08 -0300

    Objects commit.

---
+++ objects/objects.js
@@ -0,0 +1,28 @@
+// var person = {
+//   gender : 'Male',
+//   'eyecolor' : 'Brown'
+// };
+//
+// person.firstName = 'Filipe';
+// person.lastName = 'Morais';
+// person.age = 29;
+//
+// delete person.age;
+// console.log(person);
+//
+// function greetUser (person) {
+//   console.log('Hello ' + person.firstName + ' ' + person.lastName + '!');
+// }
+//
+// greetUser(person);
+
+var pet = {
+  name : 'Lummy',
+  type : 'cat'
+}
+
+function printPet (pet) {
+  console.log('You have a ' + pet.type + ' is named ' + pet.name + '.');
+}
+
+printPet (pet);
